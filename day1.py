7feb0c779e306999607322ee700567456b424a6d
John Suico <johncdsuico@gmail.com>
2023-01-05 10:34:54 -0800

    Finish day 1 exercise

---
+++ day1.py
@@ -0,0 +1,9 @@
+# Goal: To create a band name generator
+# Ask the user what city they grew up in.
+# Ask the user what their pet's name is.
+# Concatenate the strings together to create the band name.
+
+print("Welcome to the Band Name Generator")
+city = input("What's the name of the city you grew up in?\n")
+petName = input("What's your pet's name?\n")
+print("Your band name could be " + city + " " + petName)
