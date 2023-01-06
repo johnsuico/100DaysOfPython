9bfc9fd3ea8f504a9b6a32e022654b4c04bc38a4
John Suico <johncdsuico@gmail.com>
2023-01-06 11:02:37 -0800

    Fix index out of range

--- Day4/rock_paper_scissors.py
+++ Day4/rock_paper_scissors.py
@@ -34,25 +34,28 @@ def rock_paper_scissors() :
     print("1. Rock")
     print("2. Paper")
     print("3. Scissors")
-    user_choice = int(input("Please enter the corresponding number."))-1
+    user_choice = int(input("Please enter the corresponding number.\n"))-1
 
     computer_choice = random.randint(0, 2)
 
-    print("You chose: ")
-    print(game_images[user_choice])
-
-    print("Computer chose:")
-    print(game_images[computer_choice])
-
     if (user_choice >= 4 or user_choice <= 0) :
         print("You didn't enter in a valid choice.")
-    elif (user_choice == 1 and computer_choice == 2) :
-        print("You lost.")
-    elif (user_choice == 1 and computer_choice == 3) :
-        print("You won.")
-    elif (user_choice > computer_choice) :
-        print("You won.")
-    elif (user_choice < computer_choice) :
-        print("You lost.")
-    elif (user_choice == computer_choice) :
-        print("It's a draw.")
+    else:
+        print("You chose: ")
+        print(game_images[user_choice])
+
+        print("Computer chose:")
+        print(game_images[computer_choice])
+
+        if (user_choice >= 4 or user_choice <= 0) :
+            print("You didn't enter in a valid choice.")
+        elif (user_choice == 1 and computer_choice == 2) :
+            print("You lost.")
+        elif (user_choice == 1 and computer_choice == 3) :
+            print("You won.")
+        elif (user_choice > computer_choice) :
+            print("You won.")
+        elif (user_choice < computer_choice) :
+            print("You lost.")
+        elif (user_choice == computer_choice) :
+            print("It's a draw.")
