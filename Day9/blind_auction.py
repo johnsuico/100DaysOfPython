13953166c64cfe94ad58281fddb70dd81482fd20
John Suico <johncdsuico@gmail.com>
2023-01-09 12:58:44 -0800

    Finished day9

---
+++ Day9/blind_auction.py
@@ -0,0 +1,40 @@
+import os
+
+clear = lambda: os.system('cls')
+clear()
+
+print("Welcome to the secret auction prgram.")
+
+more_bids = False
+bids = {}
+
+def add_bidder():
+    name = input("What is your name?: ")
+    amount = input("What's your bid?: ")
+
+    bids[name] = amount
+
+    more_bidders = input("Are there any other bidders? Type 'yes' or 'no'.\n").lower()
+
+    if more_bidders == "yes":
+        return True
+    else:
+        return False
+
+more_bids = add_bidder()
+
+while more_bids:
+    clear()
+    bidders = add_bidder()
+
+max_bid = 0
+max_bid_person = ""
+
+clear()
+
+for person in bids:
+    if int(bids[person]) > max_bid:
+        max_bid = int(bids[person])
+        max_bid_person = person
+
+print(f"{max_bid_person} has the highest bid with: ${max_bid}")
