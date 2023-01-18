cbe287ac399328a4b786c3aa47214cde5feb2aab
John Suico <johncdsuico@gmail.com>
2023-01-18 08:55:19 -0800

    Finish day 18

---
+++ Day18/main.py
@@ -0,0 +1,43 @@
+from turtle import Turtle, Screen
+import random
+
+colors = [
+    (96,66,68),     # Brown
+    (255,219,185),  # light brown
+    (160,57,38),    # Red brownish
+    (4,90,151),     # Navy blueish
+    (234,169,129),  # Skin toneish
+    (246,178,71),   # Mustrad yellowish
+    (142,134,51),   # Dark greenish
+    (154,168,19),   # Light greenish
+    (189,158,8),    # Yellow greenish
+    (189,199,174),  # Gray blue
+    (195,43,19),    # Redish
+    (246,235,207),  # light whiteish
+    (133,165,188),  # Mute blue
+]
+
+def draw_dots(num_dots, offset):
+    for i in range(num_dots):
+        t.dot(15, random.choice(colors))
+        t.fd(25)
+    
+    t.home()
+    y_pos = t.ycor()
+    t.sety(y_pos+offset)
+
+def create_painting(num_rows, num_columns):
+    offset = 25
+    for i in range(num_rows):
+        draw_dots(num_columns, offset)
+        offset += 25
+
+t = Turtle()
+t.pu()
+t.speed('fastest')
+screen = Screen()
+screen.colormode(255)
+
+create_painting(10, 10)
+
+screen.exitonclick()
