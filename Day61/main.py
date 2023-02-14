7c0ecffec8b2b2bdeb941e4c6bc1345bf459d019
John Suico <johncdsuico@gmail.com>
2023-02-13 19:48:25 -0800

    Finish day 61

---
+++ Day61/main.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+
+app = Flask(__name__)
+app.secret_key = 'testingsecretstring'
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired()])
+    password = StringField('Password', validators=[DataRequired()])
+    submit = SubmitField(label='Log In')
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+@app.route("/login")
+def login():
+    login_form = LoginForm()
+    login_form.validate_on_submit()
+    return render_template('login.html', form=login_form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
